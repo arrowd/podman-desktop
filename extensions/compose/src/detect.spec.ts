204eb86d51ab1c9b28b112c6810abbfd68f180e9
Gleb Popov <6yearold@gmail.com>
2025-04-08 10:15:10 +0300

    chore: Introduce isUnixLike field into extension API environment
    Use it throughout the codebase to unlock common codepaths for FreeBSD.
    
    Signed-off-by: Gleb Popov <6yearold@gmail.com>

--- extensions/compose/src/detect.spec.ts
+++ extensions/compose/src/detect.spec.ts
@@ -32,6 +32,7 @@ import { OS } from './os';
 const osMock: OS = {
   isWindows: vi.fn(),
   isLinux: vi.fn(),
+  isFreeBSD: vi.fn(),
   isMac: vi.fn(),
   isUnixLike: vi.fn(),
 };
@@ -192,6 +193,14 @@ describe('parseVersion', () => {
 
 describe('Check default socket path', async () => {
   test('linux', async () => {
+    (osMock.isUnixLike as Mock).mockReturnValue(true);
+    (osMock.isMac as Mock).mockReturnValue(false);
+    (osMock.isWindows as Mock).mockReturnValue(false);
+    const result = detect.getSocketPath();
+    expect(result).toBe('/var/run/docker.sock');
+  });
+
+  test('freebsd', async () => {
     (osMock.isUnixLike as Mock).mockReturnValue(true);
     (osMock.isMac as Mock).mockReturnValue(false);
     (osMock.isWindows as Mock).mockReturnValue(false);
